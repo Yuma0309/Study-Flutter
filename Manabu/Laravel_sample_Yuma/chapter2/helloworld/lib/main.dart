13c29c532decd3acc2d8d1342595ebbbbb043583
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-04-26 01:13:36 +0900

    リスト2.1までの動作を確認

---
+++ Manabu/Laravel_sample_Yuma/chapter2/helloworld/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// 1. エントリーポイントのmain関数
+void main() {
+  // 2. MyAppを呼び出す
+  runApp(const MyApp());
+}
+// MyAppのクラス
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 3. タイトルとテーマを設定する。画面の本体はMyHomePageで作る。
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Text("HelloWorld"),
+    );
+  }
+}
