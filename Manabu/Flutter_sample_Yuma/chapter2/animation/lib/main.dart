e5e7895cfa5ef5dd226789906b1d09a847a2edf1
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-05-10 23:10:41 +0900

    リスト2.37までの動作を確認

---
+++ Manabu/Flutter_sample_Yuma/chapter2/animation/lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _flag = false;
+
+  // ボタンタップ時にフラグを切り替える
+  _click() async {
+    setState(() {
+      _flag = !_flag;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedOpacity(
+              opacity: _flag ? 0.1 : 1.0,
+              duration: const Duration(seconds: 3),
+              child: Text(
+                "消える文字",
+                style: Theme.of(context).textTheme.headline4,
+              )),
+            AnimatedSize(
+              duration: const Duration(seconds: 3),
+              child: SizedBox(
+                width: _flag ? 50 : 200,
+                height: _flag ? 50 : 200,
+                child: Container(color: Colors.purple))),
+            AnimatedAlign(
+              duration: const Duration(seconds: 3),
+              alignment: _flag ? Alignment.topLeft : Alignment.bottomRight,
+              child: SizedBox(
+                width: 50,
+                height: 50,
+                child: Container(color: Colors.green)))
+          ],
+        ),
+      ),
+      floatingActionButton:
+        FloatingActionButton(onPressed: _click, child: const Icon (Icons.add)),
+    );
+  }
+}
