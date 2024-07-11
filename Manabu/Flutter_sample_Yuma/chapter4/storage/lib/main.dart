069a74f4d91bbb89371f79da264a4c21083e67c5
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-07-12 00:14:55 +0900

    リスト4.22までの動作を確認

--- Manabu/Flutter_sample_Yuma/chapter4/storage/lib/main.dart
+++ Manabu/Flutter_sample_Yuma/chapter4/storage/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -55,71 +66,81 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Image? _img;
+  Text? _text;
 
-  void _incrementCounter() {
+  // ダウンロード処理
+  Future<void> _download() async {
+    // ファイルのダウンロード
+    FirebaseStorage storage = FirebaseStorage.instance;
+    Reference imageRef = storage.ref().child("DL").child("flutter.png");
+    String imageUrl = await imageRef.getDownloadURL();
+    Reference textRef = storage.ref("DL/hello.txt");
+    var data = await textRef.getData();
+
+    // 画面に反映
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _img = Image.network(imageUrl);
+      _text = Text(ascii.decode(data!));
     });
+
+    // 画像ファイルはローカルにもを保存
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    File downloadToFile = File("${appDocDir.path}/download-logo.png");
+    try {
+      await imageRef.writeToFile(downloadToFile);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // アップロード処理
+  void _upload() async {
+    // imagePickerで画像を選択する
+    final pickerFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickerFile == null) {
+      return;
+    }
+    File file = File(pickerFile.path);
+    FirebaseStorage storage = FirebaseStorage.instance;
+    try {
+      await storage.ref("UL/upload-pic.png").putFile(file);
+      setState(() {
+        _img = null;
+        _text = const Text("UploadDone");
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ダウンロードしたイメージとテキストを表示
+            children: <Widget>[
+              if (_img != null) _img!,
+              if (_text != null) _text!,
+            ],
+          ),
+        ),
+        floatingActionButton:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          FloatingActionButton(
+            onPressed: _download,
+            child: const Icon(Icons.download_outlined),
+          ),
+          FloatingActionButton(
+            onPressed: _upload,
+            child: const Icon(Icons.upload_outlined),
+          ),
+        ]));
   }
 }
