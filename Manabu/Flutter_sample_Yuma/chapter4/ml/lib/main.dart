b9ddcf566ef958da17a46c2bf30cf474db7e794d
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-07-15 00:43:46 +0900

    リスト4.25までの動作を確認

--- Manabu/Flutter_sample_Yuma/chapter4/ml/lib/main.dart
+++ Manabu/Flutter_sample_Yuma/chapter4/ml/lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -55,71 +65,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Text? _text;
+  Image? _image;
 
-  void _incrementCounter() {
+  // OCRを行う
+  Future<void> _ocr() async {
+    final pickerFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickerFile == null) {
+      return;
+    }
+    final InputImage imageFile = InputImage.fromFilePath(pickerFile.path);
+    final textRecognizer =
+        TextRecognizer(script: TextRecognitionScript.japanese);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(imageFile);
+    String text = recognizedText.text;
+    /*
+    for (TextBlock block in recognizedText.blocks) {
+      // ブロック単位で取得したい情報がある場合はここに記載
+      for (TextLine line in block.lines) {
+        // ライン単位で取得したい情報がある場合はここに記載
+      }
+    }
+    */
+
+    // 画面に反映
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _text = Text(text);
+      _image = Image.file(File(pickerFile.path));
     });
+
+    // リソースの解放
+    textRecognizer.close();
+  }
+
+  // ラベリングを行う
+  Future<void> _labeling() async {
+    final pickerFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    if (pickerFile == null) {
+      return;
+    }
+    final InputImage imageFile = InputImage.fromFilePath(pickerFile.path);
+
+    final ImageLabelerOptions options =
+        ImageLabelerOptions(confidenceThreshold: 0.7);
+    final imageLabeler = ImageLabeler(options: options);
+    final List<ImageLabel> labels = await imageLabeler.processImage(imageFile);
+
+    String text = "";
+    for (ImageLabel label in labels) {
+      text +=
+          "${label.label} (${(label.confidence * 100).toStringAsFixed(0)}%)\n";
+    }
+
+    // 画面に反映
+    setState(() {
+      _text = Text(text);
+      _image = Image.file(File(pickerFile.path));
+    });
+
+    // リソースの解放
+    imageLabeler.close();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (_text != null) _text!,
+              if (_image != null) SafeArea(child: _image!),
+            ],
+          ),
+        ),
+        floatingActionButton:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          FloatingActionButton(
+              onPressed: _ocr, child: const Icon(Icons.photo_album)),
+          FloatingActionButton(
+              onPressed: _labeling, child: const Icon(Icons.photo_camera))
+        ]));
   }
 }
