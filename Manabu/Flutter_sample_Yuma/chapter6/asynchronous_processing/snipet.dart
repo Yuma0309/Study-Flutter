ef3a8c33a2ee01baff4623f45cc9a89abab0a1af
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-08-13 00:33:50 +0900

    リスト6.8までの動作を確認

--- Manabu/Flutter_sample_Yuma/chapter6/asynchronous_processing/snipet.dart
+++ Manabu/Flutter_sample_Yuma/chapter6/asynchronous_processing/snipet.dart
@@ -4,16 +4,17 @@ void main() {
   print("main begin");
   print(DateTime.now().toString());
   print("dart1 start");
-  print(syncFunc("data1", 5));
+  print(asyncFunc("data1", 5));
   print("dart2 start");
-  print(syncFunc("data2", 3));
+  print(asyncFunc("data2", 3));
   print("dart3 start");
-  print(syncFunc("data3", 1));
+  print(asyncFunc("data3", 1));
   print("main end");
 }
 // timeの時間分スリープし、その後現在時間を返す関数
-String syncFunc(String name, int time) {
-  // sleep(Duration(seconds: time));の代わり
-  for(int i = 0; i < time * 1000000000; i++){}
-  return "${name}:${DateTime.now().toString()}";
+Future<String> asyncFunc(String name, int time) async {
+  return Future.delayed(Duration(seconds: time), () {
+    print("${name} delay done");
+    return "${name}:${DateTime.now().toString()}";
+  });
 }
