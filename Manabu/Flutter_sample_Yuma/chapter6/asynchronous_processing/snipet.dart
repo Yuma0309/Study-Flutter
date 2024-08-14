e618968f1d0b581f27d19baafa8047aa7aa64a99
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-08-15 00:09:14 +0900

    リスト6.10までの動作を確認

--- Manabu/Flutter_sample_Yuma/chapter6/asynchronous_processing/snipet.dart
+++ Manabu/Flutter_sample_Yuma/chapter6/asynchronous_processing/snipet.dart
@@ -4,23 +4,12 @@ void main() async {
   print("main begin");
   print(DateTime.now().toString());
   print("dart1 start");
-  Future<String> result1 = asyncFunc("data1", 5);
-  result1.then((result) {
-    print(result);
-  });
+  print(await asyncFunc("data1", 3));
   print("dart2 start");
-  Future<String> result2 = asyncFunc("data2", 3);
-  result2.then((result) {
-    print(result);
-  });
+  print(await asyncFunc("data2", 2));
   print("dart3 start");
-  Future<String> result3 = asyncFunc("data3", 1);
-  result3.then((result) {
-    print(result);
-  });
-
+  print(await asyncFunc("data3", 1));
   print("main end");
-
 }
 
 // timeの時間分スリープし、その後現在時間を返す関数
