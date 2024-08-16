4d1157c5c3fc07ae30d81b6f897e4f91803a2738
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-08-16 23:34:16 +0900

    リスト6.11までの動作を確認

--- Manabu/Flutter_sample_Yuma/chapter6/asynchronous_processing/snipet.dart
+++ Manabu/Flutter_sample_Yuma/chapter6/asynchronous_processing/snipet.dart
@@ -4,14 +4,20 @@ void main() async {
   print("main begin");
   print(DateTime.now().toString());
   print("dart1 start");
-  print(await asyncFunc("data1", 3));
+  Future<String> data1 = asyncFunc("data1", 5);
   print("dart2 start");
-  print(await asyncFunc("data2", 2));
+  Future<String> data2 = asyncFunc("data2", 3);
   print("dart3 start");
-  print(await asyncFunc("data3", 1));
+  Future<String> data3 = asyncFunc("data3", 1);
   print("main end");
-}
 
+
+  // 全部のFutureを待ち合わせる
+  List<String> datalist = await Future.wait([data1,data2,data3]);
+  for(String data in datalist){
+    print(data);
+  }
+}
 // timeの時間分スリープし、その後現在時間を返す関数
 Future<String> asyncFunc(String name, int time) async {
   return Future.delayed(Duration(seconds: time), () {
