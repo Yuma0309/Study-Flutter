172664c674019c627cddacdad2dd4683bc71d29e
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-09-04 01:40:19 +0900

    リスト6.30までの動作を確認

---
+++ Manabu/Flutter_sample_Yuma/chapter6/inheritedwidget/final/inheritedwidget/lib/my_inherited_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyInheritedWidget extends InheritedWidget {
+  int counter;
+  final String message;
+  // コンストラクタでカウンタとメッセージと子Widgetを取る
+  MyInheritedWidget(
+      {Key? key,
+      required this.counter,
+      required this.message,
+      required Widget child})
+      : super(key: key, child: child);
+
+  // O(1)でInheritedWidgetを返却
+  // 依存関係を登録し変更時に再構築されるようにする
+  static MyInheritedWidget of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<MyInheritedWidget>()
+          as MyInheritedWidget;
+
+  //更新されたかどうかの判定ロジック
+  @override
+  bool updateShouldNotify(MyInheritedWidget oldWidget) =>
+      oldWidget.counter != counter;
+}
