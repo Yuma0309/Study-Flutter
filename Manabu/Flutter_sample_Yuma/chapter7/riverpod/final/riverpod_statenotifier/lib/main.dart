ed4e738730044a177d45300396a71b0decde7471
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-09-23 04:48:38 +0900

    リスト7.17までの動作を確認

--- Manabu/Flutter_sample_Yuma/chapter7/riverpod/final/riverpod_statenotifier/lib/main.dart
+++ Manabu/Flutter_sample_Yuma/chapter7/riverpod/final/riverpod_statenotifier/lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'mydata.dart';
+
+// 1-1. グローバル変数にProviderを定義する
+final _mydataProvider = StateNotifierProvider<MyData, double>((ref) => MyData());
 
 void main() {
-  runApp(const MyApp());
+  // 1-2. ProviderScopeを設定する
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -55,71 +65,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+      body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            // 2-1. Text用にConsumerを使う
+            Consumer(builder: (context, ref, child) {
+              return Text(
+                // 2-2.refを用いてstateの値を取り出す
+                ref.watch(_mydataProvider).toStringAsFixed(2),
+                style: const TextStyle(fontSize: 100),
+              );
+            }),
+            // 3-1. Slider用にConsumerを使う
+            Consumer(builder: (context, ref, child) {
+              return Slider(
+                // 3-2.refを用いてstateの値を取り出す
+                value: ref.watch(_mydataProvider),
+                // 3-3.changeStateで状態を変える
+                onChanged: (value) =>
+                  ref.read(_mydataProvider.notifier).changeState(value));
+            }),
           ],
-        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
